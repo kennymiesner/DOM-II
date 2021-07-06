3c6f6aa39b06fd70bdf810c2a380b8573ab61f2f
kennymiesner <kennymiesner@gmail.com>
2021-07-06 15:19:16 -0500

    Added 2. Onmouseover

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 // 1. Onclick
 const logo = document.querySelector('.logo-heading')
 
-logo.onclick = function changeContent() {
+logo.addEventListener('click', function (event) {
     logo.textContent = "Fun Bus Travel Agency";
     logo.style.color = "#17A2B8";
-}
+})
+
+// 2. Onmouseover
+const footer = document.querySelector('footer')
+
+footer.addEventListener('mouseover', function (event) {
+    footer.style.backgroundColor = "#fff"
+})
+
