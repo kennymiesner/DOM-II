757abcb655c4d57dcba461f7ebe9aba1555385f0
kennymiesner <kennymiesner@gmail.com>
2021-07-06 15:25:27 -0500

    Added onmousedown, onmouseup, onmouseover, onmouseleave

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
-// 1. Onclick
+// 1. Onmousedown
 const logo = document.querySelector('.logo-heading')
 
-logo.addEventListener('click', function (event) {
-    logo.textContent = "Fun Bus Travel Agency";
-    logo.style.color = "#17A2B8";
+logo.addEventListener('mousedown', function (event) {
+    logo.textContent = "Fun Bus Travel Agency"
+    logo.style.color = "#17A2B8"
 })
 
-// 2. Onmouseover
+// 2. Onmouseup
+logo.addEventListener('mouseup', function (event) {
+    logo.textContent = "Fun Bus"
+    logo.style.color = "#000"
+})
+
+// 3. Onmouseover
 const footer = document.querySelector('footer')
 
 footer.addEventListener('mouseover', function (event) {
     footer.style.backgroundColor = "#fff"
 })
 
+// 4. Onmouseleave
+footer.addEventListener('mouseleave', function (event) {
+    footer.style.backgroundColor = "#FFEBCD"
+})
