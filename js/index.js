a31199224c982659dcdf7ea893ea5bc95e5c9f67
kennymiesner <kennymiesner@gmail.com>
2021-07-06 15:04:53 -0500

    Added 1. Onclick

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+// 1. Onclick
+const logo = document.querySelector('.logo-heading')
+
+logo.onclick = function changeContent() {
+    logo.textContent = "Fun Bus Travel Agency";
+    logo.style.color = "#17A2B8";
+}
