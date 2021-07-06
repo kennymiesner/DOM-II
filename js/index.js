a8aa8fedb67a2c899e88097795e682542864f811
kennymiesner <kennymiesner@gmail.com>
2021-07-06 15:39:48 -0500

    Added onclick / remove

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 // 1. Onmousedown
 const logo = document.querySelector('.logo-heading')
 
-logo.addEventListener('mousedown', function (event) {
+logo.addEventListener('mousedown', function () {
     logo.textContent = "Fun Bus Travel Agency"
     logo.style.color = "#17A2B8"
 })
 
 // 2. Onmouseup
-logo.addEventListener('mouseup', function (event) {
+logo.addEventListener('mouseup', function () {
     logo.textContent = "Fun Bus"
     logo.style.color = "#000"
 })
@@ -15,11 +15,18 @@ logo.addEventListener('mouseup', function (event) {
 // 3. Onmouseover
 const footer = document.querySelector('footer')
 
-footer.addEventListener('mouseover', function (event) {
+footer.addEventListener('mouseover', function () {
     footer.style.backgroundColor = "#fff"
 })
 
 // 4. Onmouseleave
-footer.addEventListener('mouseleave', function (event) {
+footer.addEventListener('mouseleave', function () {
     footer.style.backgroundColor = "#FFEBCD"
+})
+
+// 5. Onclick / Remove
+let image = document.querySelector('.intro img')
+
+image.addEventListener('click', function () {
+    image.remove();
 })
